8cd4355abc368897b3675d614e6cf2f1a00f6d59
Ajay Kumar <104864110+ajaythink@users.noreply.github.com>
2025-06-02 10:33:24 +0530

    voice recognition working

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,103 @@
-import Image from "next/image";
+"use client";
+import { useState, useEffect } from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [isClient, setIsClient] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  const startListning = () =>
+    SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
+
+  const { transcript, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser does not support speech recognition.</span>;
+  }
+
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    if (transcript) {
+      const words = transcript.trim().split(/\s+/);
+      setCount(words.length);
+    }
+  }, [transcript]);
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className=" text-center text-2xl font-bold m-4">Voice Counter App</h1>
+      <p className="text-6xl font-bold text-green-500 m-4 border-2 rounded-full p-2 bg-blue-50">{count}</p>
+      <div className="flex flex-row gap-3 p-4">
+        <button
+          className="cursor-pointer border rounded-2xl p-2 bg-blue-500 text-white"
+          onClick={startListning}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          Start Recognition
+        </button>
+
+        <button
+          className="cursor-pointer border rounded-2xl p-2 bg-red-500 text-white"
+          onClick={SpeechRecognition.stopListening}
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          Stop Recognition
+        </button>
+      </div>
+      {/* <textarea className="border w-full p-2 text-black">
+        {transcript}
+      </textarea> */}
+      <textarea
+        value={transcript}
+        readOnly
+        className="border w-full p-2 text-black bg-gray-200"
+      />
     </div>
   );
 }
+
+// const speechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+// const recognition = new speechRecognition();
+
+// let recognition;
+
+// if (typeof window !== "undefined") {
+//   const speechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+//   recognition = new speechRecognition();
+// }
+
+// const [recoStop, setRecoStop] = useState(true);
+
+// const [text, setText] = useState("Please press the above button to start voice recognition.");
+
+// const [count, setCount] =useState(0);
+
+// recognition.start=() =>{
+//   setText("Listening...");
+// }
+
+// const handleClick = () =>{
+//   if(recoStop) {
+//     recognition.start();
+//     setRecoStop(false);
+//     setText("Listening...");
+//     // recognition.onresult = (event) => {
+//     //   const result = event.results[0][0].transcript;
+//     //   setText(result);
+//     //   setCount(result.split(" ").length);
+//     // };
+//     // recognition.onerror = (event) => {
+//     //   console.error("Error occurred in recognition: " + event.error);
+//     //   setText("Error occurred, please try again.");
+//     // };
+//   }
+//   else{
+//     recognition.stop();
+//     setRecoStop(true);
+//     setText("Recognition stopped. Press the button to start again.");
+//   }
+// }
