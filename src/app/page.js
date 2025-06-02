64a93adc9cd60016ad33dcb5bc2713f025b5a088
Ajay Kumar <104864110+ajaythink@users.noreply.github.com>
2025-06-02 13:24:30 +0530

    mic on and off added and counter circle created and app name changed and app discritption updated

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,10 @@ import { useState, useEffect } from "react";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
+import { FaMicrophone } from "react-icons/fa6";
+import { FaMicrophoneSlash } from "react-icons/fa";
+
+
 
 export default function Home() {
   const [count, setCount] = useState(0);
@@ -24,34 +28,35 @@ export default function Home() {
     SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className=" text-center text-2xl font-bold m-4">Voice Counter App</h1>
-      <p className="text-6xl font-bold text-green-500 m-4 border-2 rounded-full p-2 bg-blue-50">
-        {count}
-      </p>
-      <div className="flex flex-row gap-3 p-4">
-        <button
-          className="cursor-pointer border rounded-2xl p-2 bg-blue-500 text-white"
-          onClick={startListning}
-        >
-          Start Recognition
-        </button>
+    <div className=" min-h-screen bg-blue-500">
+      <h1 className=" text-center text-2xl font-bold p-2 ">Naam Jaap App</h1>
+      <div className="flex flex-col items-center justify-center  bg-white border rounded-2xl border-pink-800 m-4">
+        <p className="text-7xl font-bold text-green-500 m-4 rounded-full w-[200px] h-[200px] bg-yellow-300 text-center items-center flex justify-center">
+          {count}
+        </p>
+        <div className="flex flex-row gap-3 p-4">
+          <button
+            className="cursor-pointer border rounded-full p-2 bg-blue-900 text-white text-3xl "
+            onClick={startListning}
+          >
+            <FaMicrophone />
+          </button>
 
-        <button
-          className="cursor-pointer border rounded-2xl p-2 bg-red-500 text-white"
-          onClick={SpeechRecognition.stopListening}
-        >
-          Stop Recognition
-        </button>
+          <button
+            className="cursor-pointer border rounded-full p-2 bg-red-500 text-white text-3xl"
+            onClick={SpeechRecognition.stopListening}
+          >
+            <FaMicrophoneSlash />
+          </button>
+        </div>
+        <div className=" w-full p-2">
+          <textarea
+            value={transcript}
+            readOnly
+            className="w-full p-2 text-black bg-gray-200 focus:outline-none rounded-lg h-32"
+          />
+        </div>
       </div>
-      {/* <textarea className="border w-full p-2 text-black">
-        {transcript}
-      </textarea> */}
-      <textarea
-        value={transcript}
-        readOnly
-        className="border w-full p-2 text-black bg-gray-200"
-      />
     </div>
   );
 }
