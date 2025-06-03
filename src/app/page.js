86b147bef25c6ef5e0604af8d966d5f0ae5d2c99
Ajay Kumar <104864110+ajaythink@users.noreply.github.com>
2025-06-03 12:35:52 +0530

    Mic button updated and made a single Icon when active or Inactive that time only one icon will be visible

--- src/app/page.js
+++ src/app/page.js
@@ -6,13 +6,15 @@ import SpeechRecognition, {
 import { FaMicrophone } from "react-icons/fa6";
 import { FaMicrophoneSlash } from "react-icons/fa";
 
-
-
 export default function Home() {
   const [count, setCount] = useState(0);
 
   const { transcript, browserSupportsSpeechRecognition } =
     useSpeechRecognition();
+  const [isActive, setIsActive] = useState(true);
+  const [userMsg, setuserMsg] = useState(
+    "Press the microphone button to start counting your naam jaap."
+  );
 
   useEffect(() => {
     if (transcript) {
@@ -24,30 +26,48 @@ export default function Home() {
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser does not support speech recognition.</span>;
   }
-  const startListning = () =>
+  const startListning = () => {
+    setIsActive(false);
+    setuserMsg("Listening... Press the microphone button to stop.");
     SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
+  };
+
+  const stopListning = () => {
+    setIsActive(true);
+    setuserMsg("Press the microphone button to start counting your naam jaap.");    
+    SpeechRecognition.stopListening();
+  };
 
   return (
     <div className=" min-h-screen bg-blue-500">
-      <h1 className=" text-center text-2xl font-bold p-2 ">Naam Jaap App</h1>
-      <div className="flex flex-col items-center justify-center  bg-white border rounded-2xl border-pink-800 m-4">
+      <h1 className=" text-center text-2xl font-bold p-2 text-white">
+        Naam Jaap App
+      </h1>
+      <div className="flex flex-col items-center justify-center  bg-white  rounded-2xl  m-4">
+        <p>{userMsg}</p>
         <p className="text-7xl font-bold text-green-500 m-4 rounded-full w-[200px] h-[200px] bg-yellow-300 text-center items-center flex justify-center">
           {count}
         </p>
         <div className="flex flex-row gap-3 p-4">
-          <button
-            className="cursor-pointer border rounded-full p-2 bg-blue-900 text-white text-3xl "
-            onClick={startListning}
-          >
-            <FaMicrophone />
-          </button>
-
-          <button
-            className="cursor-pointer border rounded-full p-2 bg-red-500 text-white text-3xl"
-            onClick={SpeechRecognition.stopListening}
-          >
-            <FaMicrophoneSlash />
-          </button>
+          {isActive ? (
+            <div
+              className="border-gray-400 hover:bg-gray-200 border rounded-full cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+              onClick={startListning}
+            >
+              <button className=" text-gray-700 text-3xl ">
+                <FaMicrophone />
+              </button>
+            </div>
+          ) : (
+            <div
+              className="border-gray-400 hover:bg-gray-200 border rounded-full cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+              onClick={stopListning}
+            >
+              <button className="  text-red-800 text-4xl ">
+                <FaMicrophoneSlash />
+              </button>
+            </div>
+          )}
         </div>
         <div className=" w-full p-2">
           <textarea
